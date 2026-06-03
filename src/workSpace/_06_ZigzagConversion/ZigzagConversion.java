ac7084f936cb7df3c0472aa39e25e62ac695e83f
SIJIN LEE <22011161@sju.ac.kr>
2026-06-03 19:43:40 +0900

    06. Zigzag Conversion

---
+++ src/workSpace/_06_ZigzagConversion/ZigzagConversion.java
@@ -0,0 +1,11 @@
+package workSpace._06_ZigzagConversion;
+class Solution {
+    public String convert(String s, int numRows) {
+
+    }
+}
+public class ZigzagConversion {
+    public static void main(String[] args) {
+
+    }
+}
