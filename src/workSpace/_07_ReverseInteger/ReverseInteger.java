83e267b1f5bc1035710d02ce7dad9aa14f3fd7c1
SIJIN LEE <22011161@sju.ac.kr>
2026-06-13 15:50:52 +0900

    ReverseInteger

---
+++ src/workSpace/_07_ReverseInteger/ReverseInteger.java
@@ -0,0 +1,15 @@
+package workSpace._07_ReverseInteger;
+import java.util.Scanner;
+class Solution {
+    public int reverse(int x) {
+        return 0;
+    }
+}
+public class ReverseInteger {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        System.out.println(s.reverse(x));
+    }
+}
